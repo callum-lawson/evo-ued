17de2d9fd59b9701d8c1d105f3dd01ee21f6522f
callum-lawson <callumrlawson@gmail.com>
2025-08-20 12:10:10 -0700

    Add explantory comments to DR loss function

--- examples/maze_dr_egt.py
+++ examples/maze_dr_egt.py
@@ -276,38 +276,41 @@ def update_actor_critic_rnn(
             def loss_fn(params):
                 _, pi, values_pred = train_state.apply_fn(
                     params, (obs, last_dones), init_hstate
-                )
-                log_probs_pred = pi.log_prob(actions)
-                entropy = pi.entropy().mean()
+                ) # forward pass
+                log_probs_pred = pi.log_prob(actions) # action probabilites
+                entropy = pi.entropy().mean() # pull towards entropy
 
                 ratio = jnp.exp(log_probs_pred - log_probs)
 
                 # TODO: add environment importance sampling
+                # Use jax softmax verison?
 
                 A = (advantages - advantages.mean()) / (advantages.std() + 1e-5)
+                # normalize advantages
                 l_clip = (
                     -jnp.minimum(
                         ratio * A, jnp.clip(ratio, 1 - clip_eps, 1 + clip_eps) * A
                     )
-                ).mean()
+                ).mean() # policy clipping AND mean loss
 
                 values_pred_clipped = values + (values_pred - values).clip(
                     -clip_eps, clip_eps
-                )
+                ) # value function clipping
                 l_vf = (
                     0.5
                     * jnp.maximum(
                         (values_pred - targets) ** 2,
                         (values_pred_clipped - targets) ** 2,
                     ).mean()
-                )
+                ) # value function mean loss
 
                 loss = l_clip + critic_coeff * l_vf - entropy_coeff * entropy
+                # policy + value + entropy
 
                 return loss, (l_vf, l_clip, entropy)
 
             grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-            loss, grads = grad_fn(train_state.params)
+            loss, grads = grad_fn(train_state.params) # get loss and gradients
             if update_grad:
                 train_state = train_state.apply_gradients(grads=grads)
             return train_state, loss
@@ -315,6 +318,7 @@ def update_actor_critic_rnn(
         rng, train_state = carry
         rng, rng_perm = jax.random.split(rng)
         permutation = jax.random.permutation(rng_perm, num_envs)
+
         minibatches = (
             jax.tree_map(
                 lambda x: jnp.take(x, permutation, axis=0).reshape(
@@ -329,6 +333,8 @@ def update_actor_critic_rnn(
                 batch,
             ),
         )
+        # n_env / n_minibatch envs per minibatch
+        # default envs is 32 (n_minibatch = 1)
         train_state, losses = jax.lax.scan(update_minibatch, train_state, minibatches)
         return (rng, train_state), losses
 
